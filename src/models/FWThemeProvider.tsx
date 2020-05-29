596a6d65033b69e43adc5749a41b3a619f7994fe
Deric Walintukan <deric@loopapps.com>
2020-05-28 18:55:22 -0700

    Add theme to fw theme provider

--- src/models/FWThemeProvider.tsx
+++ src/models/FWThemeProvider.tsx
@@ -1,13 +1,28 @@
 import React from 'react'
 import { ThemeProvider } from './ThemeProvider'
-import fwTheme from '../theme/fw'
+import fwThemeDark from '../theme/fw'
+import fwThemeLight from '../theme/fwLight'
+import type { Theme } from '../theme/theme'
 
 type Props = {
+  theme?: string
   children: React.ReactNode
 }
 
-export const FWThemeProvider: React.FC<Props> = ({ children }: Props) => {
-  return <ThemeProvider theme={fwTheme}>{children}</ThemeProvider>
+const mapTheme = (theme): Theme => {
+  switch (theme) {
+    case 'light':
+      return fwThemeLight
+    default:
+      return fwThemeDark
+  }
+}
+
+export const FWThemeProvider: React.FC<Props> = ({
+  theme,
+  children
+}: Props) => {
+  return <ThemeProvider theme={mapTheme(theme)}>{children}</ThemeProvider>
 }
 
 FWThemeProvider.displayName = 'FWThemeProvider'
---
+++ src/theme/fwLight.ts
@@ -0,0 +1,124 @@
+import type { Theme } from './theme'
+
+const fontFamily =
+  'Avenir Next, -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Helvetica Neue, Arial, sans-serif'
+
+const colors = {
+  primaryGradient: 'linear-gradient(254deg, #f78361, #f54b64)',
+  primary: '#ff7061',
+  blk1: '#121212',
+  gray1: '#171717',
+  gray2: '#2b2b2b',
+  gray3: '#404040',
+  gray4: '#9b9b9b',
+  gray5: '#bdbdbd',
+  white: '#ffffff'
+}
+
+const fontSizes = {
+  xxsmall: '10px',
+  xsmall: '12px',
+  small: '14px',
+  medium: '16px',
+  large: '18px',
+  xlarge: '20px',
+  xxlarge: '24px',
+  xxxlarge: '30px'
+}
+
+const fontOffsets = {
+  xxsmall: ['1px', '3px'],
+  xsmall: ['1px', '4px'],
+  small: ['2px', '4px'],
+  medium: ['2px', '5px'],
+  large: ['3px', '6px'],
+  xlarge: ['4px', '6px'],
+  xxlarge: ['5px', '7px'],
+  xxxlarge: ['6px', '8px']
+}
+
+const fwTheme: Theme = {
+  colors: colors,
+  fontSizes: fontSizes,
+  fontOffsets: fontOffsets,
+  button: {
+    bg: 'transparent',
+    borderRadius: '500px',
+    color: 'white',
+    fontFamily: fontFamily,
+    weight: 'semiBold',
+    lineHeight: 1.5,
+    '&:hover': {
+      color: 'white',
+      textDecoration: 'none'
+    },
+    '&:focus': {
+      color: 'white',
+      textDecoration: 'none'
+    },
+    '&:disabled': {
+      opacity: 0.3
+    }
+  },
+  input: {
+    bg: colors['blk1'],
+    borderRadius: '12px',
+    boxShadow: `0px 0px 0px 1px ${colors['gray3']} inset`,
+    color: 'white',
+    fontFamily: fontFamily,
+    '::placeholder': {
+      color: colors['gray5']
+    },
+    '&:focus': {
+      outline: 'none',
+      boxShadow: `0px 0px 0px 2px ${colors['primary']} inset`
+    }
+  },
+  text: {
+    fontFamily: fontFamily,
+    weight: 'medium'
+  },
+  heading: {
+    fontFamily: fontFamily,
+    weight: 'bold'
+  },
+  variants: {
+    primary: {
+      backgroundImage: colors['primaryGradient']
+    },
+    outline: {
+      boxShadow: 'inset 0 0 0 1px #4b4b4b',
+      '&:hover': {
+        bg: 'rgba(255, 255, 255, 0.2)',
+        boxShadow: 'none',
+        color: 'white',
+        textDecoration: 'none'
+      }
+    },
+    icon: {
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+      flexShrink: 0,
+      borderRadius: '50%',
+      height: 37,
+      width: 37,
+      p: 'none',
+      boxShadow: 'inset 0 0 0 1px #4b4b4b',
+      '&:hover': {
+        bg: 'rgba(255, 255, 255, 0.2)',
+        boxShadow: 'none',
+        color: 'white',
+        textDecoration: 'none'
+      }
+    },
+    text: {
+      '&:hover': {
+        color: 'white',
+        textDecoration: 'underline'
+      }
+    }
+  }
+}
+
+export default fwTheme

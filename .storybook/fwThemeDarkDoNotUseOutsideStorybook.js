791a20453e67304d75a04999a89c6cb2f4de7f5d
Deric Walintukan <deric@loopapps.com>
2020-05-29 22:35:25 -0700

    Rename dark storybook and add light version

--- .storybook/fwThemeDarkDoNotUseOutsideStorybook.js
+++ .storybook/fwThemeDarkDoNotUseOutsideStorybook.js
@@ -1,9 +1,9 @@
 import merge from 'lodash/merge'
 import cloneDeep from 'lodash/cloneDeep'
 import defaultTheme from '../src/theme/theme'
-import fwTheme from '../src/theme/fw'
+import fwThemeDark from '../src/theme/fwDark'
 
 // To get the Story Source to render the jsx markup in stories while iterating
 // over theme keys, sometimes we need access to the fwTheme without getting it
 // the proper way with useTheme. This is a hack just for storybook documentation.
-export default merge(cloneDeep(defaultTheme), fwTheme)
+export default merge(cloneDeep(defaultTheme), fwThemeDark)

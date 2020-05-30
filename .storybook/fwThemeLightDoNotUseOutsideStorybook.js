078ab7100350505281871874e7bc7e19832f1649
Deric Walintukan <deric@loopapps.com>
2020-05-29 22:36:23 -0700

    Rename fw theme light ref

--- .storybook/fwThemeLightDoNotUseOutsideStorybook.js
+++ .storybook/fwThemeLightDoNotUseOutsideStorybook.js
@@ -1,9 +1,9 @@
 import merge from 'lodash/merge'
 import cloneDeep from 'lodash/cloneDeep'
 import defaultTheme from '../src/theme/theme'
-import fwLight from '../src/theme/fwLight'
+import fwThemeLight from '../src/theme/fwLight'
 
 // To get the Story Source to render the jsx markup in stories while iterating
 // over theme keys, sometimes we need access to the fwTheme without getting it
 // the proper way with useTheme. This is a hack just for storybook documentation.
-export default merge(cloneDeep(defaultTheme), fwLight)
+export default merge(cloneDeep(defaultTheme), fwThemeLight)
